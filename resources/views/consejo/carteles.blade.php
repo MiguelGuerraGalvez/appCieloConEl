3fa7800078bb0f214c76e693ff8670c0fd390963
Miguel <guerragalvezmiguel@gmail.com>
2025-05-19 12:45:17 +0200

    fondo carteles, itinerarios y pregones

--- resources/views/consejo/carteles.blade.php
+++ resources/views/consejo/carteles.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Carteles')
 
 @section('image1')
-    <section class="bg-[url(./img/HEADER_ENLACE_HERMANDADES.jpg)] w-full h-[25vh] md:h-[50vh] lg:h-[75vh]"> <!-- La imagen estaría en el fondo de pantalla -->
+    <section class="bg-no-repeat bg-cover bg-[position:0_70%] w-full h-[25vh] md:h-[50vh] lg:h-[75vh]" style="background-image: url(../img/HEADER_CARTELES.jpg);"> <!-- La imagen estaría en el fondo de pantalla -->
     </section>
 @endsection
 
--- resources/views/consejo/itinerarios.blade.php
+++ resources/views/consejo/itinerarios.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Itinerarios')
 
 @section('image1')
-    <section class="bg-[url(./img/HEADER_ENLACE_HERMANDADES.jpg)] w-full h-[25vh] md:h-[50vh] lg:h-[75vh]"> <!-- La imagen estaría en el fondo de pantalla -->
+    <section class="bg-no-repeat bg-cover bg-center w-full h-[25vh] md:h-[50vh] lg:h-[75vh]" style="background-image: url(../img/HEADER_ITINERARIOS.jpg);"> <!-- La imagen estaría en el fondo de pantalla -->
     </section>
 @endsection
 
