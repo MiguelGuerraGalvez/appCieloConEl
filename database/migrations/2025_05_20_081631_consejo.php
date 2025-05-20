57523d17f407b1f90bec07bd79101bc1df6e5510
Miguel <guerragalvezmiguel@gmail.com>
2025-05-20 10:38:24 +0200

    Tablas de la base de datos

---
+++ database/migrations/2025_05_20_081631_consejo.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consejo', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_usuario');
+            $table->string('nombre');
+            $table->text('nombre_completo');
+            $table->text('escudo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consejo');
+    }
+};
---
+++ database/migrations/2025_05_20_082049_itinerarios.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('itinerarios', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_hermandad');
+            $table->string('dia');
+            $table->text('recorrido');
+            $table->text('imagen');
+            $table->boolean('aceptado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('itinerarios');
+    }
+};
