3cbff52ca147ad9a3353a8a7b84f48b509d726c6
Jesús Gómez García <jesus.gomez-garcia@iesruizgijon.com>
2025-06-12 22:08:39 +0200

    Progreso Estilos Paginas Contraseñas

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,16 +1,17 @@
+<title>Confirmar Contraseña</title>
 <link rel="icon" type="image/png" href="{{ asset('img/LOGO.png') }}">
 
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-    </div>
-
+<x-guest-layout bodyClass="bg-[position:0_17%]" bodyStyle="background-image: url('/img/FONDO_REGISTRO.jpg');">
     <form method="POST" action="{{ route('password.confirm') }}">
         @csrf
 
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('Por favor, confirma tu contraseña antes de continuar.') }}
+        </div>
+
         <!-- Password -->
         <div>
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Contraseña')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -20,9 +21,9 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <div class="flex justify-end mt-4">
+        <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Confirm') }}
+                {{ __('Confirmar') }}
             </x-primary-button>
         </div>
     </form>
--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,28 +1,41 @@
-<title>Contraseña Olvidada</title>
+<title>Recuperar Contraseña</title>
 <link rel="icon" type="image/png" href="{{ asset('img/LOGO.png') }}">
 
-<x-guest-layout>
+<x-guest-layout bodyClass="bg-center" bodyStyle="background-image: url('/img/FONDO_INICIO_SESION.jpg');">
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Olvidaste tu contraseña? No hay problema. Indica tu correo electrónico y te enviaremos un enlace para restablecerla.') }}
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    @if (session('status'))
+        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+            {{ session('status') }}
+        </div>
+    @endif
 
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Correo Electrónico')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Enviar enlace de recuperación') }}
             </x-primary-button>
+        </div>
+
+        <div class="flex items-center justify-between text-center mt-4">
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                {{ __('Volver al inicio de sesión') }}
+            </a>
+
+            <a class="underline font-bold text-sm text-gray-900 dark:text-gray-400 hover:text-black dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                {{ __('Regístrate Aquí') }}
+            </a>
         </div>
     </form>
 </x-guest-layout>
